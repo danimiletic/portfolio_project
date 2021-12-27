cc4fa5f92f0d50c7622a7964c0308ed5480dac13
dani miletic <danimiletic@gmail.com>
2021-12-27 16:12:50 -0700

    finished contact controller

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,2 +1,42 @@
 class Api::ContactsController < ApplicationController
+
+  def index 
+    @contacts = Contact.all 
+    render json: @contacts
+  end
+
+  def show 
+    @contact = Contact.find(params[:id])
+    render json: @contact
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      render json: @contact
+    else 
+      render json: { errors: @contact.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def updated
+    @contact = Contact.find(params[:id])
+    if @contact.update(contact_params)
+      render json: @contact
+    else
+      render json: { errors: @contact.errors }, status: :unprocessable_entity
+    end
+  end 
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    render json: { message: 'Contact is deleted.'}
+  end
+
+  private 
+
+    def contact_params
+      params.require(:contact).permit( :name, :email, :phone, :comment )
+    end
 end
